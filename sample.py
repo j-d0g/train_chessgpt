033b8991ab65943546e6f584216e5f5f4206319c
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2025-03-08 19:20:23 +0000

    modify sampling out_dir default value

--- sample.py
+++ sample.py
@@ -10,7 +10,7 @@ from model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-out_dir = 'out-chess-mps' # ignored if init_from is not 'resume'
+out_dir = 'out-shakespeare-char' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 5 # number of samples to draw
 max_new_tokens = 200 # number of tokens generated in each sample
